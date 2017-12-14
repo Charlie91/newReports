1ec055f87e8712825b013f4136fa9aa5fb3e131d
Алексей <gaidarzhi@iMac-admin.local>
2017-12-14 15:58:05 +0300

    1.19.9

--- src/views/ObjectPage/Bar.js
+++ src/views/ObjectPage/Bar.js
@@ -21,10 +21,137 @@ export default function BarChart(props){
     }];
 
     if(props.data){
-        bar = [];
+        bar = [
+            {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'Понедельник',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: []
+                    }
+                ]
+            },
+            {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'My First dataset',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: []
+                    }
+                ]
+            },
+            {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'My First dataset',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: []
+                    }
+                ]
+            },
+            {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'My First dataset',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: []
+                    }
+                ]
+            },
+            {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'My First dataset',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: []
+                    }
+                ]
+            },
+            {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'My First dataset',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: []
+                    }
+                ]
+            },
+            {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'My First dataset',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: []
+                    }
+                ]
+            }];
+
         props.data.weekAvg.forEach( (item,i) => {
-            console.log(item)
-        })
+            if(i < 8){
+                bar[0].labels.push(item.ld + '-' + item.td);
+                bar[0].datasets[0].data.push(item.avg);
+            }
+            else if(i < 16){
+                bar[1].labels.push(item.ld + '-' + item.td);
+                bar[1].datasets[0].data.push(item.avg);
+            }
+            else if(i < 24){
+                bar[2].labels.push(item.ld + '-' + item.td);
+                bar[2].datasets[0].data.push(item.avg);
+            }
+            else if(i < 32){
+                bar[3].labels.push(item.ld + '-' + item.td);
+                bar[3].datasets[0].data.push(item.avg);
+            }
+            else if(i < 40){
+                bar[4].labels.push(item.ld + '-' + item.td);
+                bar[4].datasets[0].data.push(item.avg);
+            }
+            else if(i < 48){
+                bar[5].labels.push(item.ld + '-' + item.td);
+                bar[5].datasets[0].data.push(item.avg);
+            }
+            else{
+                bar[6].labels.push(item.ld + '-' + item.td);
+                bar[6].datasets[0].data.push(item.avg);
+            }
+        });
+        console.log(bar)
     }
 
 
@@ -33,13 +160,17 @@ export default function BarChart(props){
         <Card className="average_hours">
             <CardBody>
                 <h4>Средняя посещаемость по часам</h4>
-                <div className="chart-wrapper" style={{display:'inline-block',maxWidth:'400px'}}>
-                    <Bar data={bar[0]}
-                         options={{
-                             maintainAspectRatio: false
-                         }}
-                    />
-                </div>
+                {bar.map(item =>
+                    <div className="chart-wrapper" style={{display:'inline-block',maxWidth:'14.2%'}}>
+                        <Bar data={item}
+                             options={{
+                                 maintainAspectRatio: false
+                             }}
+                        />
+                    </div>
+                )}
+
+
             </CardBody>
         </Card>
     )
