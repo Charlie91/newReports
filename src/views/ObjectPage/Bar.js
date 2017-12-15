655d5330053f88bb4914c534cd758bb9acaa049f
Charlie91 <aleksand44@yandex.ru>
2017-12-15 15:39:49 +0300

    j24jj1e12

--- src/views/ObjectPage/Bar.js
+++ src/views/ObjectPage/Bar.js
@@ -3,8 +3,6 @@ import {Bar, Line} from "react-chartjs-2";
 import {Row,Col,CardColumns, Card, CardHeader, CardBody} from "reactstrap";
 
 export default function BarChart(props){
-
-
     let bar = [{
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [
@@ -19,6 +17,7 @@ export default function BarChart(props){
             }
         ]
     }];
+    let maxVal = 0;
 
     if(props.data){
         bar = [
@@ -26,12 +25,13 @@ export default function BarChart(props){
                 labels: [],
                 datasets: [
                     {
-                        label: 'Понедельник',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
-                        borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        backgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        borderColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        borderWidth: 10,
+                        radius:14,
+                        hoverBorderWidth: 13,
+                        hoverBackgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        hoverBorderColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
                         data: []
                     }
                 ]
@@ -40,12 +40,11 @@ export default function BarChart(props){
                 labels: [],
                 datasets: [
                     {
-                        label: 'My First dataset',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
+                        backgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        borderColor: 'transparent',
                         borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        hoverBackgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        hoverBorderColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
                         data: []
                     }
                 ]
@@ -54,12 +53,11 @@ export default function BarChart(props){
                 labels: [],
                 datasets: [
                     {
-                        label: 'My First dataset',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
+                        backgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        borderColor: 'transparent',
                         borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        hoverBackgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        hoverBorderColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
                         data: []
                     }
                 ]
@@ -68,12 +66,11 @@ export default function BarChart(props){
                 labels: [],
                 datasets: [
                     {
-                        label: 'My First dataset',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
+                        backgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        borderColor: 'transparent',
                         borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        hoverBackgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        hoverBorderColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
                         data: []
                     }
                 ]
@@ -82,12 +79,11 @@ export default function BarChart(props){
                 labels: [],
                 datasets: [
                     {
-                        label: 'My First dataset',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
+                        backgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        borderColor: 'transparent',
                         borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        hoverBackgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        hoverBorderColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
                         data: []
                     }
                 ]
@@ -96,12 +92,11 @@ export default function BarChart(props){
                 labels: [],
                 datasets: [
                     {
-                        label: 'My First dataset',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
+                        backgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        borderColor: 'transparent',
                         borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        hoverBackgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        hoverBorderColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
                         data: []
                     }
                 ]
@@ -110,18 +105,18 @@ export default function BarChart(props){
                 labels: [],
                 datasets: [
                     {
-                        label: 'My First dataset',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
+                        backgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        borderColor: 'transparent',
                         borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        hoverBackgroundColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
+                        hoverBorderColor: ["#8570ce", '#688bda', "#74c2e8", "#73d2ca", "#9fd473", '#f0e238','#ea9772','#e07c95'],
                         data: []
                     }
                 ]
             }];
 
         props.data.weekAvg.forEach( (item,i) => {
+            if(maxVal < item.avg)maxVal = item.avg;
             if(i < 8){
                 bar[0].labels.push(item.ld + '-' + item.td);
                 bar[0].datasets[0].data.push(item.avg);
@@ -151,91 +146,89 @@ export default function BarChart(props){
                 bar[6].datasets[0].data.push(item.avg);
             }
         });
-        console.log(bar)
+        console.log(bar,maxVal);
     }
-
 
 
     return (
         <Card className="average_hours">
             <CardBody>
                 <h4>Средняя посещаемость по часам</h4>
-                {bar.map((item,i) =>{
-                    if(i === 0)
-                        return (
-                            <div key={i} className="chart-wrapper" style={{display:'inline-block',maxWidth:'14.2%',height:'212px',position:'relative',top:'6px'}}>
-                                <Bar data={item}
-                                     options={{
-                                         maintainAspectRatio: false,
-                                         legend:{
-                                             display:false,
-                                         },
-                                         scales: {
-                                             display:false,
-                                             xAxes: [{
-                                                 display:false,
-                                                 gridLines: {
-                                                     color: "rgba(0, 0, 0, 0)",
-                                                     display:false
+                {console.log(maxVal,bar)}
+                {
+                    bar.map((item,i) =>{
+                            if(i === 0)
+                                return (
+                                    <div key={i} className="chart-wrapper" style={{display:'inline-block',maxWidth:'14.2%',height:'212px',position:'relative',top:'6px',padding:'0px 15px'}}>
+                                        <Bar data={item}
+                                             options={{
+                                                 maintainAspectRatio: false,
+                                                 legend:{
+                                                     display:false,
+                                                 },
+                                                 scales: {
+                                                     display:false,
+                                                     xAxes: [{
+                                                         display:false,
+                                                         gridLines: {
+                                                             color: "rgba(0, 0, 0, 0)",
+                                                             display:false
+                                                         }
+                                                     }],
+                                                     yAxes: [{
+                                                         ticks: {
+
+                                                             beginAtZero: true,
+                                                             steps: 10,
+                                                             stepValue: 5,
+                                                             max: maxVal
+                                                         },
+                                                         gridLines: {
+                                                             color: "rgba(0, 0, 0, 0.05)",
+                                                         },
+                                                     }]
                                                  }
-                                             }],
-                                             yAxes: [{
-                                                 ticks: {
-
-                                                     beginAtZero: true,
-                                                     steps: 10,
-                                                     stepValue: 5,
-                                                     max: 100
+                                             }}
+                                        />
+                                    </div>
+                                );
+                            else
+                                return(
+                                    <div key={i} className="chart-wrapper" style={{display:'inline-block',maxWidth:'14.2%',height:'200px',padding:'0px 15px'}}>
+                                        <Bar data={item}
+                                             options={{
+                                                 maintainAspectRatio: false,
+                                                 legend:{
+                                                     display:false,
                                                  },
-                                                 gridLines: {
-                                                     color: "rgba(0, 0, 0, 0.05)",
-                                                 },
-                                             }]
-                                         }
-                                     }}
-                                />
-                            </div>
-                        );
-                    else
-                        return(
-                            <div key={i} className="chart-wrapper" style={{display:'inline-block',maxWidth:'14.2%',height:'200px'}}>
-                                <Bar data={item}
-                                     options={{
-                                         maintainAspectRatio: false,
-                                         legend:{
-                                             display:false,
-                                         },
-                                         scales: {
-                                             display:false,
-                                             xAxes: [{
-                                                 display:false,
-                                                 gridLines: {
-                                                     color: "rgba(0, 0, 0, 0)",
-                                                     display:false
+                                                 scales: {
+                                                     display:false,
+                                                     xAxes: [{
+                                                         display:false,
+                                                         gridLines: {
+                                                             color: "rgba(0, 0, 0, 0)",
+                                                             display:false
+                                                         }
+                                                     }],
+                                                     yAxes: [{
+                                                         ticks: {
+                                                             display: false,
+                                                             beginAtZero: true,
+                                                             steps: 10,
+                                                             stepValue: 5,
+                                                             max: maxVal
+                                                         },
+                                                         gridLines: {
+                                                             color: "rgba(0, 0, 0, 0.05)",
+                                                         },
+                                                     }]
                                                  }
-                                             }],
-                                             yAxes: [{
-                                                 ticks: {
-                                                     display: false,
-                                                     beginAtZero: true,
-                                                     steps: 10,
-                                                     stepValue: 5,
-                                                     max: 100
-                                                 },
-                                                 gridLines: {
-                                                     color: "rgba(0, 0, 0, 0.05)",
-                                                 },
-                                             }]
-                                         }
-                                     }}
-                                />
-                            </div>
-                        )
-                }
-
-                )}
-
-
+                                             }}
+                                        />
+                                    </div>
+                                )
+                        }
+                    )}
             </CardBody>
         </Card>
     )
