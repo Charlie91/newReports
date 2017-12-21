f5192ef178e6c3be91d2e96669e84df7b35b0020
krill221 <krill221@gmail.com>
2017-12-21 18:21:43 +0300

    clear element iphone tap area fix 2

--- src/views/Registration/inputs/ClearField.js
+++ src/views/Registration/inputs/ClearField.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 export default function ClearField(props){
-    //if(!props.render)return null;
+    if(!props.render)return null;
     return(
         <button className="clear-field" tabIndex="-1" onClick={props.clearField}>	</button>
     )
