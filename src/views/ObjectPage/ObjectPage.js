09e748031b1f56baed9ec832f1e0550bc5c2eafb
krill221 <krill221@gmail.com>
2018-01-25 15:15:44 +0300

    chart days show fix

--- src/views/ObjectPage/ObjectPage.js
+++ src/views/ObjectPage/ObjectPage.js
@@ -477,7 +477,8 @@ export default class ObjectPage extends Component {
                             </CardBody>
                         </Card>
                     </Col>
-                    <Col style={{overflow:'hidden', backgroundImage: (state.images.length ? `url(${API.imgPath}/${state.object.id}/${state.images[0]})` : "url(img/rio_full.jpg)") }} className="img_wrapper order-1 order-md-12" md="6" xs="12">
+                    <Col style={{overflow:'hidden', backgroundImage: (state.images.length ? `url(${API.imgPath}/${state.object.id}/${state.images[0]})` : "url(img/rio_full.jpg)") }}
+                         className="img_wrapper order-1 order-md-12" md="6" xs="12">
                     </Col>
                 </Row>
 
