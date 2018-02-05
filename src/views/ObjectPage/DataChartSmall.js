7c3f9f22cb866db0b1dd3e03627f190164882c49
Charlie91 <aleksand44@yandex.ru>
2018-02-05 15:05:31 +0300

    Исправлены баги с нижним десктопным и мобильным графиками реструктурированы их компоненты

--- src/views/ObjectPage/DataChartSmall.js
+++ src/views/ObjectPage/DataChartSmall.js
@@ -8,198 +8,164 @@ import {customLabel3} from "./customtooltip3";
 import {formatNumericValueWithMnl, getStepName, getStepSize, getStepTick} from "../../utils/utils";
 import moment from "moment/moment";
 
-export default class DataChartSmall extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            chart :{
-                labels: [],
-                datasets: [{
-                    data: []
-                }]
-            },
-            startDate: null,
-            endDate: null,
-            currency: null,
-            timeSegment: null,
-            emptyData: null
-
-        }
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.data)
-            this.setChartWithDataSet(nextProps.data);
-
-        if (nextProps.startDate)
-            this.setState({startDate: nextProps.startDate});
-
-        if (nextProps.endDate)
-            this.setState({endDate: nextProps.endDate});
-
-        if (nextProps.currency)
-            this.setState({currency: nextProps.currency});
-
-        if (nextProps.timeSegment)
-            this.setState({timeSegment: nextProps.timeSegment});
-
-        if (nextProps.emptyData)
-            this.setState({emptyData: nextProps.emptyData});
-
-    }
 
 
-    setChartWithDataSet(chart){
+function addAdditionalStylesToChart(chart) {
+    if(chart.datasets.length > 1){
         chart.datasets[1].pointBorderWidth = 2.3;
         chart.datasets[1].pointHoverRadius = 3;
         chart.datasets[1].pointRadius = 2;
         chart.datasets[0].borderWidth = 2;
         chart.datasets[1].borderWidth = 2;
-        this.setState({chart: chart});
     }
+    return chart;
+}
 
-    render(){
-        return (
-            <Col  md='12' style={{padding:'0px'}} className="order-12 order-md-1">
-        {this.state.emptyData ? <p className="error-message">Отсутствуют данные</p> : ''}
-        <div style={this.state.emptyData ? {display:'none'} : {}} className="line-chart-wrapper line-chart-wrapper_small">
-            {(!this.state.chart.datasets[0].data.length) ?
-                <Loading/>
-                :
-                <div className="linechart_area_wrapper_small">
-                    <Line data={this.state.chart}
-                          options={{
-                              maintainAspectRatio: false,
-                              animation: {
-                                  duration: 700,
-                              },
-                              legend: {
-                                  display: false
-                              },
-                              tooltips: {
-                                  custom: customLabel3,
-                                  enabled:false,
-                                  callbacks:{
-                                      label: (tooltipItem, data ) => {
-                                          return `${formatNumberBySpaces(Math.round(tooltipItem.yLabel))} ${this.state.currency.substring(0,3)}.`
-                                      }
-                                  }
-                              },
-                              scales: {
-                                  xAxes: [
-                                      {
-                                          id: 'main-x-axis',
-                                          afterFit: function (scale) {
-                                              scale.height = 29;
-                                          },
-                                          type: 'time',
-                                          time: {
-                                              unit: getStepTick(this.state.timeSegment),
-                                              unitStepSize: getStepSize(this.state.chart.labels.length, this.state.timeSegment),
-                                              displayFormats: {
-                                                  day: getStepName(this.state.timeSegment),
-                                              }
-                                          },
-                                          display: true,
-                                          ticks: {
-                                              beginAtZero:false,
-                                              padding: 12,
-                                              fontColor:'#7f8fa4',
-                                              fontSize: 12,
-                                              fontFamily: 'ProximaNova',
-                                              callback: (value, index, values) => {
-                                                  if (!moment(value).isValid()){
-                                                      return '';
-                                                  }
 
-                                                  let side = ( (index === 0) || (index === (values.length -1)) );
-                                                  let step = getStepSize(this.state.chart.labels.length, this.state.timeSegment);
-                                                  let len = Math.ceil(this.state.chart.labels.length / step);
-                                                  // if end
-                                                  if(index === 0){
-                                                      return ( side && (len - values.length) < 1 ) ? '' :
-                                                          moment(value).format( getStepName(this.state.timeSegment) );
-                                                  }
-                                                  // if end
-                                                  if(index === (values.length -1)){
-                                                      return ( side && (len - values.length) < 2 ) ? '' :
-                                                          moment(value).format( getStepName(this.state.timeSegment) );
-                                                  }
-
-                                                  return moment(value).format( getStepName(this.state.timeSegment) );
-                                              }
-                                          },
-                                          gridLines: {
-                                              drawBorder: true,
-                                              drawOnChartArea: false,
-                                              drawTicks:false
+const DataChartSmall = (props) => {
+    let newChart = addAdditionalStylesToChart(props.data);
+    return (
+        <Col  md='12' style={{padding:'0px'}} className="order-12 order-md-1">
+            {props.emptyData ? <p className="error-message">Отсутствуют данные</p> : ''}
+            <div style={props.emptyData ? {display:'none'} : {}} className="line-chart-wrapper line-chart-wrapper_small">
+                {(!newChart.datasets[0].data.length) ?
+                    <Loading/>
+                    :
+                    <div className="linechart_area_wrapper_small">
+                        <Line data={newChart}
+                              options={{
+                                  maintainAspectRatio: false,
+                                  animation: {
+                                      duration: 700,
+                                  },
+                                  legend: {
+                                      display: false
+                                  },
+                                  tooltips: {
+                                      custom: customLabel3,
+                                      enabled:false,
+                                      callbacks:{
+                                          label: (tooltipItem, data ) => {
+                                              return `${formatNumberBySpaces(Math.round(tooltipItem.yLabel))} ${props.currency.substring(0,3)}.`
                                           }
-                                      },
-                                      {
-                                          id: "main-x-axis2",
+                                      }
+                                  },
+                                  scales: {
+                                      xAxes: [
+                                          {
+                                              id: 'main-x-axis',
+                                              afterFit: function (scale) {
+                                                  scale.height = 29;
+                                              },
+                                              type: 'time',
+                                              time: {
+                                                  unit: getStepTick(props.timeSegment),
+                                                  unitStepSize: getStepSize(newChart.labels.length, props.timeSegment),
+                                                  displayFormats: {
+                                                      day: getStepName(props.timeSegment),
+                                                  }
+                                              },
+                                              display: true,
+                                              ticks: {
+                                                  beginAtZero:false,
+                                                  padding: 12,
+                                                  fontColor:'#7f8fa4',
+                                                  fontSize: 12,
+                                                  fontFamily: 'ProximaNova',
+                                                  callback: (value, index, values) => {
+                                                      if (!moment(value).isValid()){
+                                                          return '';
+                                                      }
+
+                                                      let side = ( (index === 0) || (index === (values.length -1)) );
+                                                      let step = getStepSize(newChart.labels.length, props.timeSegment);
+                                                      let len = Math.ceil(newChart.labels.length / step);
+                                                      // if end
+                                                      if(index === 0){
+                                                          return ( side && (len - values.length) < 1 ) ? '' :
+                                                              moment(value).format( getStepName(props.timeSegment) );
+                                                      }
+                                                      // if end
+                                                      if(index === (values.length -1)){
+                                                          return ( side && (len - values.length) < 2 ) ? '' :
+                                                              moment(value).format( getStepName(props.timeSegment) );
+                                                      }
+
+                                                      return moment(value).format( getStepName(props.timeSegment) );
+                                                  }
+                                              },
+                                              gridLines: {
+                                                  drawBorder: true,
+                                                  drawOnChartArea: false,
+                                                  drawTicks:false
+                                              }
+                                          },
+                                          {
+                                              id: "main-x-axis2",
+                                              gridLines: {
+                                                  drawBorder: false,
+                                                  drawOnChartArea: false,
+                                                  drawTicks:false
+                                              },
+                                              type: "time",
+                                              time: {
+                                                  unit: getStepTick(props.timeSegment),
+                                                  unitStepSize: getStepSize(newChart.labels.length, props.timeSegment),
+                                                  displayFormats: {
+                                                      day: "YYYY"
+                                                  }
+                                              },
+                                              display: ( (props.startDate.format('YYYY') !== props.endDate.format('YYYY')) &&
+                                                  ((props.timeSegment === 'D') || (props.timeSegment === 'M'))
+                                              ),
+                                              ticks: {
+                                                  beginAtZero:false,
+                                                  padding: 0,
+                                                  fontColor:'#7f8fa4',
+                                                  fontSize: 12,
+                                                  fontFamily: 'ProximaNova',
+                                                  callback: (value, index, values) => {
+                                                      let side = ( (index === 0) || (index === (values.length -1)) );
+                                                      let step = getStepSize(newChart.labels.length, props.timeSegment);
+                                                      let len = Math.ceil(newChart.labels.length / step);
+
+                                                      // if end
+                                                      if(index === 0){
+                                                          return ( side && (len - values.length) < 1 ) ? '' :
+                                                              moment(value).format('YYYY');
+                                                      }
+                                                      // if end
+                                                      if(index === (values.length -1)){
+                                                          return ( side && (len - values.length) < 2 ) ? '' :
+                                                              moment(value).format('YYYY');
+                                                      }
+
+                                                      return moment(value).format('YYYY');
+                                                  }
+                                              }
+                                          }
+                                      ],
+                                      yAxes: [{
+                                          ticks: {
+                                              display: false,
+                                              beginAtZero: true,
+                                              padding: 0,
+
+                                          },
                                           gridLines: {
                                               drawBorder: false,
                                               drawOnChartArea: false,
                                               drawTicks:false
                                           },
-                                          type: "time",
-                                          time: {
-                                              unit: getStepTick(this.state.timeSegment),
-                                              unitStepSize: getStepSize(this.state.chart.labels.length, this.state.timeSegment),
-                                              displayFormats: {
-                                                  day: "YYYY"
-                                              }
-                                          },
-                                          display: ( (this.state.startDate.format('YYYY') !== this.state.endDate.format('YYYY')) &&
-                                              ((this.state.timeSegment === 'D') || (this.state.timeSegment === 'M'))
-                                          ),
-                                          ticks: {
-                                              beginAtZero:false,
-                                              padding: 0,
-                                              fontColor:'#7f8fa4',
-                                              fontSize: 12,
-                                              fontFamily: 'ProximaNova',
-                                              callback: (value, index, values) => {
-                                                  let side = ( (index === 0) || (index === (values.length -1)) );
-                                                  let step = getStepSize(this.state.chart.labels.length, this.state.timeSegment);
-                                                  let len = Math.ceil(this.state.chart.labels.length / step);
-
-                                                  // if end
-                                                  if(index === 0){
-                                                      return ( side && (len - values.length) < 1 ) ? '' :
-                                                          moment(value).format('YYYY');
-                                                  }
-                                                  // if end
-                                                  if(index === (values.length -1)){
-                                                      return ( side && (len - values.length) < 2 ) ? '' :
-                                                          moment(value).format('YYYY');
-                                                  }
-
-                                                  return moment(value).format('YYYY');
-                                              }
-                                          }
-                                      }
-                                  ],
-                                  yAxes: [{
-                                      ticks: {
-                                          display: false,
-                                          beginAtZero: true,
-                                          padding: 0,
-
-                                      },
-                                      gridLines: {
-                                          drawBorder: false,
-                                          drawOnChartArea: false,
-                                          drawTicks:false
-                                      },
-                                  }]
-                              }
-                          }}
-                    />
-                </div>
-            }
-        </div>
+                                      }]
+                                  }
+                              }}
+                        />
+                    </div>
+                }
+            </div>
         </Col>
-        )
-    }
-}
+    );
+};
+
+export default DataChartSmall;
